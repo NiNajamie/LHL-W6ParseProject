23f59a18fd415db24d1ad1806a29b917f5b0caf6
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-06-09 11:25:53 -0700

    added MapView

--- LHL-W6ParseProject/AddViewController.m
+++ LHL-W6ParseProject/AddViewController.m
@@ -13,48 +13,57 @@
 @end
 
 @implementation AddViewController {
-    CLLocationManager *locationManager;
+//    CLLocationManager *locationManager;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    locationManager = [CLLocationManager new];
     
-    locationManager = [CLLocationManager new];
+    MKPointAnnotation *lougheed = [[MKPointAnnotation alloc] init];
+    lougheed.title = @"Lougheed Town Centre";
+//    lougheed.subtitle = @"... and Launch Academy";
+    lougheed.coordinate = CLLocationCoordinate2DMake(49.248502, -122.896991);
+    
+    [self.mapView addAnnotation:lougheed];
+
+    
+    
 }
 
-- (IBAction)getLocationPressed:(UIButton *)sender {
+//- (IBAction)getLocationPressed:(UIButton *)sender {
+//
+//    // locationManager is the object that provides the location data
+//    locationManager.delegate = self;
+//    
+//    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+//    
+//    // get current location data continuously
+//    [locationManager startUpdatingLocation];
+//}
 
-    // locationManager is the object that provides the location data
-    locationManager.delegate = self;
-    
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    
-    // get current location data continuously
-    [locationManager startUpdatingLocation];
-}
-
-#pragma mark - CLLocationManagerDelegate
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(nonnull NSError *)error {
-    NSLog(@"didFailWithError: %@", error);
-    
-    UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:@"Error" message:@"Failed to get your current location" preferredStyle:UIAlertControllerStyleAlert];
-    
-    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
-        
-    }];
-    // add action to UIAlertController
-    [errorAlert addAction:ok];
-}
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
-    NSLog(@"didUpdateToLocation: %@", newLocation);
-    CLLocation *currentLocation = newLocation;
-    
-    if (currentLocation != nil) {
-        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
-        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
-    }
-}
+//#pragma mark - CLLocationManagerDelegate
+//- (void)locationManager:(CLLocationManager *)manager didFailWithError:(nonnull NSError *)error {
+//    NSLog(@"didFailWithError: %@", error);
+//    
+//    UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:@"Error" message:@"Failed to get your current location" preferredStyle:UIAlertControllerStyleAlert];
+//    
+//    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
+//        
+//    }];
+//    // add action to UIAlertController
+//    [errorAlert addAction:ok];
+//}
+//
+//- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+//    NSLog(@"didUpdateToLocation: %@", newLocation);
+//    CLLocation *currentLocation = newLocation;
+//    
+//    if (currentLocation != nil) {
+//        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+//        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
+//    }
+//}
 
 - (IBAction)saveButtonPressed:(UIButton *)sender {
 }
@@ -65,14 +74,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
 #pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
 @end
--- LHL-W6ParseProject/ViewController.m
+++ LHL-W6ParseProject/ViewController.m
@@ -73,4 +73,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    
+    
+}
+
+
 @end
