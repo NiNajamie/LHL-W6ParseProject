a893121b428fe189638ce165304a5556c8df00e2
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-06-09 15:40:27 -0700

    display latitude and longitude

--- LHL-W6ParseProject/AddViewController.m
+++ LHL-W6ParseProject/AddViewController.m
@@ -10,60 +10,84 @@
 
 @interface AddViewController ()
 
+@property (nonatomic) CLLocationManager *locationManager;
+//@property (nonatomic) CLGeocoder *geocoder;
+@property (nonatomic) CLPlacemark *placemark;
+
 @end
 
-@implementation AddViewController {
-//    CLLocationManager *locationManager;
-}
+@implementation AddViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    locationManager = [CLLocationManager new];
+    
+    [self.locationManager requestAlwaysAuthorization];
     
     MKPointAnnotation *lougheed = [[MKPointAnnotation alloc] init];
-    lougheed.title = @"Lougheed Town Centre";
-//    lougheed.subtitle = @"... and Launch Academy";
+    lougheed.title = @"Lougheed Town Centre, Burnaby, Canada";
     lougheed.coordinate = CLLocationCoordinate2DMake(49.248502, -122.896991);
     
+    // add pin in the map
     [self.mapView addAnnotation:lougheed];
+    
+    // CLLocationCoordinate2D contains a geographical coordinate
+    CLLocationCoordinate2D aLocation = lougheed.coordinate;
+    
+    // set distance from aLocation, 200x200<- too close
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(aLocation, 2000, 2000);
+    
+    // Adjusts the aspect ratio of the specified region to ensure that it fits in the map view’s frame.
+    MKCoordinateRegion adjustRegion = [self.mapView regionThatFits:viewRegion];
+    [self.mapView setRegion:adjustRegion animated:TRUE];
+    
+    // A Boolean value indicating whether the map should try to display the user’s location.
+    self.mapView.showsUserLocation = TRUE;
+    
+    
+//    // prepare: converting the latitude/logitude into address
+//    self.locationManager = [[CLLocationManager alloc]init];
 
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
     
-    
+    // Placemark data can be information such as the country, state, city, and street address.
+    [geocoder geocodeAddressString:lougheed.title completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        // get latitude/logitude from the map
+        CLPlacemark *firstPlace = placemarks.firstObject;
+//        NSLog(@"%@", firstPlace.location);
+        
+        self.latitudeLabel.text = [NSString stringWithFormat:@"  %.8f", firstPlace.location.coordinate.latitude];
+        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", firstPlace.location.coordinate.longitude];
+        NSLog(@"Latitude: %@, Longitude: %@", self.latitudeLabel.text, self.longitudeLabel.text);
+        
+        [geocoder reverseGeocodeLocation:firstPlace.location
+                       completionHandler:^(NSArray *placemarks, NSError *error)
+         {
+             if (error) {
+                 NSLog(@"Geocode failed with error: %@", error);
+                 return;
+             }
+             
+             NSMutableString *string = [[NSMutableString alloc] init];
+             
+             
+             
+//           if ( !=nil) {
+//           [NSMutableString appendString:]
+//           }
+             
+             self.addressLabel.text = [NSMutableString stringWithFormat:@" %@ %@\n %@ %@\n %@\n %@",firstPlace.addressDictionary[@"Name"], firstPlace.thoroughfare, firstPlace.postalCode, firstPlace.locality, firstPlace.administrativeArea, firstPlace.country];
+             
+             NSLog(@"%@ %@\n %@ %@\n %@\n %@",firstPlace.addressDictionary, firstPlace.thoroughfare, firstPlace.postalCode, firstPlace.locality, firstPlace.administrativeArea, firstPlace.country);
+             
+         }];
+        
+
+    }];
 }
 
-//- (IBAction)getLocationPressed:(UIButton *)sender {
-//
-//    // locationManager is the object that provides the location data
-//    locationManager.delegate = self;
-//    
-//    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-//    
-//    // get current location data continuously
-//    [locationManager startUpdatingLocation];
-//}
 
-//#pragma mark - CLLocationManagerDelegate
-//- (void)locationManager:(CLLocationManager *)manager didFailWithError:(nonnull NSError *)error {
-//    NSLog(@"didFailWithError: %@", error);
-//    
-//    UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:@"Error" message:@"Failed to get your current location" preferredStyle:UIAlertControllerStyleAlert];
-//    
-//    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){
-//        
-//    }];
-//    // add action to UIAlertController
-//    [errorAlert addAction:ok];
-//}
-//
-//- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
-//    NSLog(@"didUpdateToLocation: %@", newLocation);
-//    CLLocation *currentLocation = newLocation;
-//    
-//    if (currentLocation != nil) {
-//        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
-//        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
-//    }
-//}
+#pragma mark - CLLocationManagerDelegate
+
 
 - (IBAction)saveButtonPressed:(UIButton *)sender {
 }
---
+++ LHL-W6ParseProject/RoomTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoomTableViewController.h
+//  LHL-W6ParseProject
+//
+//  Created by Asuka Nakagawa on 2016-06-09.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomTableViewController : UITableViewController
+
+@end
